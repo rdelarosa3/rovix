c1c9b05d40d94d3b29d7c3697980499f98ceb22d
Vixtrum <vixtrumchoo@yahoo.com>
2018-09-09 21:53:07 +0800

    Done carrierwave , dont know how to use mini-magick

--- Gemfile
+++ Gemfile
@@ -39,7 +39,7 @@ gem 'carrierwave', '~> 1.0'
 
 
 # Use ActiveStorage variant
-# gem 'mini_magick', '~> 4.8'
+gem 'mini_magick', '~> 4.8'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,16 @@ class User < ApplicationRecord
 
          has_many :watchlists,dependent: :destroy
 
+        mount_uploader :avatar, AvatarUploader
+
   def full_name
     first_name + " " + last_name
-    
   end
+
+  
+  def profile_pic
+    return self.avatar.variant(resize: '300x300')
+  end
+  
+  
 end
